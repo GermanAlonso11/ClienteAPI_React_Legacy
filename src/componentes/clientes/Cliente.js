134289b64e11ffd66076bfc046771be2e85e7c75
German Alonso <german.aguiniga11@outlook.es>
2025-09-05 23:33:34 -0700

    wip 3

--- src/componentes/clientes/Cliente.js
+++ src/componentes/clientes/Cliente.js
@@ -11,7 +11,7 @@ function Cliente({cliente}) {
     const {_id, nombre, apellido, empresa, email, telefono} = cliente;
 
     //Eliminar cliente con mensajes de confirmación de sweetalert2 y axios
-    //WIP 2
+    //WIP 3
     const eliminarCliente = id => {
         //Preguntar al usuario si está seguro de eliminar el cliente
         Swal.fire({
